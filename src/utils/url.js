7fe26b20b0ff7c79a9cf21ee12da2be1a683ea3a
naviti-it <naviti.it@gmail.com>
2023-05-28 12:42:09 +0300

    change url

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,2 +1,4 @@
+// export const API_URL =
+//     process.env.PUBLIC_API_URL || "http://127.0.0.1:1337";
 export const API_URL =
-    process.env.PUBLIC_API_URL || "http://127.0.0.1:1337";
+"https://e-commerce-app-strapi.netlify.app";
