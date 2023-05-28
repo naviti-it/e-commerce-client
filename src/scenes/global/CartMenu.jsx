ec605c27d98188811fe241ee65054f3e7c1683ce
naviti-it <naviti.it@gmail.com>
2023-05-29 01:20:25 +0300

    fix itemDetail

--- src/scenes/global/CartMenu.jsx
+++ src/scenes/global/CartMenu.jsx
@@ -70,7 +70,7 @@ const CartMenu = () => {
                                             alt={item?.name}
                                             width='123px'
                                             height='164px'
-                                            src={`${API_URL}${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`} />
+                                            src={item?.attributes?.image?.data?.attributes?.formats?.medium?.url} />
                                     </Box>
                                     <Box flex='1 1 60%'>
                                         {/* ITEM NAME */}
