7e4be295660d43cc13120e111a3f6098e5da2887
naviti-it <naviti.it@gmail.com>
2023-05-28 12:57:14 +0300

    fix url

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,4 +1,4 @@
 // export const API_URL =
 //     process.env.PUBLIC_API_URL || "http://127.0.0.1:1337";
 export const API_URL =
-"https://e-commerce-app-strapi.netlify.app";
+"https://e-commerce-store-4uq7.onrender.com";
