417785ef41dcaacc3e311ca7b3175fd95259ce74
naviti-it <naviti.it@gmail.com>
2023-06-08 15:26:56 +0300

    fix api_url

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,5 +1,7 @@
 
-export const API_URL = process.env.PUBLIC_API_URL;
+// export const API_URL = process.env.PUBLIC_API_URL;
+export const API_URL =
+"https://e-commerce-store-4uq7.onrender.com";
 
 export const STRAPI_API_TOKEN = process.env.PUBLIC_STRAPI_API_TOKEN;
 
