ed1c251e4b2b61be7dbb1a1095d2c9971488b4b7
naviti-it <naviti.it@gmail.com>
2023-07-03 18:50:30 +0300

    delete shopingList

--- src/scenes/home/Home.jsx
+++ src/scenes/home/Home.jsx
@@ -1,11 +1,9 @@
 import MainCarousel from './MainCarousel'
-import ShopingList from './ShopingList'
 import Subscribe from './Subscribe'
 
 const Home = () => {
     return <div className='home'>
         <MainCarousel />
-        <ShopingList />
         <Subscribe />
     </div>
 }
