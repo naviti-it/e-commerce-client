21432c8f35ee83549a9e30730ce865009046a784
naviti-it <naviti.it@gmail.com>
2023-07-03 18:47:45 +0300

    change shopingList

--- src/scenes/home/ShopingList.jsx
+++ src/scenes/home/ShopingList.jsx
@@ -1,21 +1,18 @@
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Box, Typography, Tab, Tabs, useMediaQuery, LinearProgress } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import Item from '../../components/Item';
 import { setItems } from '../../state'
 import { API_URL } from './../../utils/url';
+import Loader from './Loader';
 
 const ShopingList = () => {
 
     const dispatch = useDispatch();
-    const [value, setValue] = useState('all');
     const [loading, setLoading] = useState(true)
     const items = useSelector((state) => state.cart.items);
-    const isNonMobile = useMediaQuery('(min-width:600px)');
+    const filteredItems = useSelector((state) => state.cart.filteredItems)
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue)
-    }
 
     async function getItems() {
         setLoading(true)
@@ -27,7 +24,6 @@ const ShopingList = () => {
                 }
             );
 
-
             const itemsJson = await items.json();
             setLoading(false)
             dispatch(setItems(itemsJson.data))
@@ -36,56 +32,27 @@ const ShopingList = () => {
             setLoading(false)
             console.log(error)
         }
-
     }
 
     useEffect(() => {
         getItems();
     }, []);
 
-    const topRatedItems = items.filter(
-        (item) => item.attributes.category === 'topRated'
-    );
-    const newArrivalsItems = items.filter(
-        (item) => item.attributes.category === 'newArrivals'
-    );
-    const bestSellersItems = items.filter(
-        (item) => item.attributes.category === 'bestSellers'
-    );
-
     return (
         <Box
             width='80%'
-            margin='80px auto'
+            margin='100px auto'
         >
             <Typography
                 variant='h3'
                 textAlign='center'
+                marginBottom='20px'
             >
                 Our Feature <b>Products</b>
             </Typography>
-            <Tabs
-                textColor='primary'
-                indicatorColor='primary'
-                value={value}
-                onChange={handleChange}
-                centered
-                TabIndicatorProps={{
-                    sx: { display: isNonMobile ? 'block' : 'none' }
-                }}
-                sx={{
-                    m: '25px',
-                    '& .MuiTabs-flexContainer': {
-                        flexWrap: 'wrap'
-                    }
-                }}
-            >
-                <Tab label='All' value='all' />
-                <Tab label='NEW ARRIVALS' value='newArrivals' />
-                <Tab label='BEST SELLERS' value='bestSellers' />
-                <Tab label='TOP RATED' value='topRated' />
-            </Tabs>
-            {loading && <LinearProgress />}
+
+            {loading && <Loader />}
+
             <Box
                 margin='1 auto'
                 display='grid'
@@ -93,20 +60,14 @@ const ShopingList = () => {
                 justifyContent='space-around'
                 rowGap='20px'
                 columnGap='1.33%'
-
             >
-                {value === 'all' && items.map((item) => (
-                    <Item item={item} key={`${item.name} - ${item.id}`} />
-                ))}
-                {value === 'newArrivals' && newArrivalsItems.map((item) => (
-                    <Item item={item} key={`${item.name} - ${item.id}`} />
-                ))}
-                {value === 'bestSellers' && bestSellersItems.map((item) => (
-                    <Item item={item} key={`${item.name} - ${item.id}`} />
-                ))}
-                {value === 'topRated' && topRatedItems.map((item) => (
-                    <Item item={item} key={`${item.name} - ${item.id}`} />
-                ))}
+                {filteredItems.length > 0 ?
+                    filteredItems?.map((item) => (
+                        <Item item={item} key={`${item.name} - ${item.id}`} />
+                    )) :
+                    items?.map((item) => (
+                        <Item item={item} key={`${item.name} - ${item.id}`} />
+                    ))}
             </Box>
         </Box>
     )
